ff51de621ddd8f92b708955c7341690a74b1aa16
peabnuts123 <peabnuts123@gmail.com>
2019-01-02 13:52:15 +1300

    Remove main index.ts module

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-// import { test } from '@app/builders/test';
-
-// console.log("Test: " , test);
-
-// const output = "Test: " + test;
-// export default output;
-let a: string = "Hello world";
-console.log(a);
