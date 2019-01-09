45d5ddf3976bacdf501302bea158520c1ffc98ec
peabnuts123 <peabnuts123@gmail.com>
2019-01-10 07:36:50 +1300

    Adding documentation to models

--- src/model/Model.ts
+++ src/model/Model.ts
@@ -1,8 +1,14 @@
 import Module from '@app/module/Module';
 
 export default abstract class Model {
+  /**
+   * The noise module that is used to generate the output values.
+   */
   public sourceModule: Module;
 
+  /**
+   * @param sourceModule The noise module that is used to generate the output values.
+   */
   public constructor(sourceModule: Module) {
     this.sourceModule = sourceModule;
   }
