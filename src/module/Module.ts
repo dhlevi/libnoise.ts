459e9a8827fd5706fad4f394c3a5a45444a68081
peabnuts123 <peabnuts123@gmail.com>
2019-01-11 17:52:27 +1300

    *Actually* documenting base module class.
    Accidentally committed wrong file in fe4b3f651522a2ae0648493841aa290d18f2e44f.

--- src/module/Module.ts
+++ src/module/Module.ts
@@ -1,4 +1,14 @@
 export default abstract class Module {
   // Functions
+  /**
+   * Generates an output value given the coordinates of the specified
+   * input value.
+   *
+   * @param x The x coordinate of the input value.
+   * @param y The y coordinate of the input value.
+   * @param z The z coordinate of the input value.
+   *
+   * @returns The output value.
+   */
   public abstract getValue(x: number, y: number, z: number): number;
 }
