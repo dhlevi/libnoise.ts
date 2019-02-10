62f748aaa5654b5829bdf5cb0f7edeca1e30f084
peabnuts123 <peabnuts123@gmail.com>
2019-02-10 20:55:25 +1300

    Update format of build number (2)

--- scripts/build/setBuildNumber.ts
+++ scripts/build/setBuildNumber.ts
@@ -9,7 +9,7 @@ import ERROR_CODES from './errorCodes';
 
     let version = buildConstants.packageJson.version;
     if (branchName !== 'master') {
-      version += `<${branchName}-${buildId}>`;
+      version += `_${branchName}-${buildId}`;
     }
 
     console.log(`##vso[build.updatebuildnumber]${version}`);
