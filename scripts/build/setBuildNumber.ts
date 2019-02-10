4213e2e5576aad06da228ca2d212a6acfc2a54f7
peabnuts123 <peabnuts123@gmail.com>
2019-02-10 15:05:56 +1300

    Fail project if set build number fails

--- scripts/build/setBuildNumber.ts
+++ scripts/build/setBuildNumber.ts
@@ -1,34 +1,43 @@
 import * as azdev from 'azure-devops-node-api';
 
 import { getBaseBuildConstants } from "../util";
+import ERROR_CODES from './errorCodes';
+
 
 (async () => {
-  const buildConstants = getBaseBuildConstants();
-  const orgUrl = "https://dev.azure.com/peabnuts123";
-  const token: string = process.env.AZURE_DEVOPS_TOKEN as string;
-  const definitionId = parseInt(process.env.SYSTEM_DEFINITIONID as string, 10);
-  const projectName = process.env.SYSTEM_TEAMPROJECT as string;
-  const branchName = process.env.BUILD_SOURCEBRANCHNAME as string;
-  const buildId = process.env.BUILD_BUILDID as string;
+  try {
 
-  const authHandler = azdev.getPersonalAccessTokenHandler(token);
-  const connection = new azdev.WebApi(orgUrl, authHandler);
-  const build = await connection.getBuildApi();
-  const buildDefinition = await build.getDefinition(definitionId, projectName);
+    const buildConstants = getBaseBuildConstants();
+    const orgUrl = "https://dev.azure.com/peabnuts123";
+    const token: string = process.env.AZURE_DEVOPS_TOKEN as string;
+    const definitionId = parseInt(process.env.SYSTEM_DEFINITIONID as string, 10);
+    const projectName = process.env.SYSTEM_TEAMPROJECT as string;
+    const branchName = process.env.BUILD_SOURCEBRANCHNAME as string;
+    const buildId = process.env.BUILD_BUILDID as string;
 
-  let version = buildConstants.packageJson.version;
-  if (branchName !== 'master') {
-    version += `-${branchName}b${buildId}`;
+    const authHandler = azdev.getPersonalAccessTokenHandler(token);
+    const connection = new azdev.WebApi(orgUrl, authHandler);
+    const build = await connection.getBuildApi();
+    const buildDefinition = await build.getDefinition(definitionId, projectName);
+
+    let version = buildConstants.packageJson.version;
+    if (branchName !== 'master') {
+      version += `-${branchName}b${buildId}`;
+    }
+
+    buildDefinition.variables = {
+      ...buildDefinition.variables,
+      VERSION: {
+        allowOverride: false,
+        isSecret: false,
+        value: version,
+      },
+    };
+
+    await build.updateDefinition(buildDefinition, definitionId, projectName);
+  } catch (e) {
+    console.error(e);
+    console.error("Failed to set build number. ");
+    process.exit(ERROR_CODES.SET_BUILD_NUMBER_FAILED);
   }
-
-  buildDefinition.variables = {
-    ...buildDefinition.variables,
-    VERSION: {
-      allowOverride: false,
-      isSecret: false,
-      value: version,
-    },
-  };
-
-  await build.updateDefinition(buildDefinition, definitionId, projectName);
 })();
